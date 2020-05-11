3b429d6494ffc5c1407bb909c68e4e56662defb6
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-05-11 14:59:43 +0100

    Add a skeleton for REDUCEConfigDialog.

---
+++ src/fjwright/runreduce/REDUCEConfigDialog.java
@@ -0,0 +1,43 @@
+package fjwright.runreduce;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class REDUCEConfigDialog {
+    public TextField defaultRootDirTextField;
+    public TextField packagesRootDirTextField;
+    public TextField docRootDirTextField;
+
+    public ListView listView;
+    public Button deleteVersionButton;
+    public Button duplicateVersionButton;
+
+    public TextField versionNameTextField;
+    public TextField versionRootDirTextField;
+    public TextField commandPathNameTextField;
+    public TextField arg1;
+    public TextField arg2;
+    public TextField arg3;
+    public TextField arg4;
+    public TextField arg5;
+
+    public void resetAllDefaultsButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteVersionButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void duplicateVersionButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void addVersionButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void saveButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+    }
+}
