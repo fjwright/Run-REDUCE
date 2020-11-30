53498f8cb96b0b4211cb6e08d98aca415ea60b54
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-11-30 18:29:08 +0000

    v2.6 release. Substantially revise the Configure REDUCE... section of the User Guide.

--- src/fjwright/runreduce/RunREDUCEFrame.java
+++ src/fjwright/runreduce/RunREDUCEFrame.java
@@ -660,7 +660,7 @@ public class RunREDUCEFrame {
         RunREDUCE.hostServices.showDocument("https://sourceforge.net/projects/reduce-algebra/");
     }
 
-    static final String VERSION = "2.58";
+    static final String VERSION = "2.6";
 
     @FXML
     private void aboutMenuItemAction() {
