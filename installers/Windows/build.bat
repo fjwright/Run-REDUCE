d48ed6cc13076fff48a78e686e3b19cc9b4bb4fa
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-12-12 18:11:17 +0000

    Update the Windows installer builder. This version works!

--- installers/Windows/build.bat
+++ installers/Windows/build.bat
@@ -1,19 +1,19 @@
-rem Use jpackage to build a Run-REDUCE installer for Windows
+@echo off
+rem Use jpackage to build a Run-REDUCE installer for Windows.
 rem User's Guide: https://docs.oracle.com/en/java/javase/15/jpackage/
 rem Man Page: https://docs.oracle.com/en/java/javase/15/docs/specs/man/jpackage.html
-rem Requires JDK 14+ and WiX 3.0+
+rem Requires JDK 14+ and WiX 3.0+. Uses JAVA_HOME.
 
-rem The launcher Run-REDUCE.exe does not yet work!
 rem --type app-image creates only the application image for testing.
-rem --add-modules javafx.fxml,javafx.web is probably not necessary.
 
 jpackage --name Run-REDUCE --app-version 2.65 ^
 --module Run.REDUCE/fjwright.runreduce.RunREDUCE ^
---module-path "D:\Program Files\AdoptOpenJDK\jdk-14.0.2.12-hotspot\jmods";%PATH_TO_FX%;C:\Users\franc\IdeaProjects\Run-REDUCE\out\production ^
---add-modules javafx.fxml,javafx.web ^
---type app-image ^
+--module-path %PATH_TO_FX_MODS%;..\..\out\production ^
+--type msi ^
 --icon RR-icon-128.ico ^
 --description "A JavaFX GUI to run the REDUCE CAS" ^
---vendor "Francis Wright -- https://github.com/fjwright/Run-REDUCE" ^
+--vendor "Francis Wright" ^
 --copyright "2020, Francis Wright, All rights reserved" ^
---verbose --temp temp
+--win-upgrade-uuid 83c944a0-27b3-43a2-99ff-455d746acabc ^
+--win-shortcut --win-dir-chooser ^
+--win-menu --win-menu-group Reduce
