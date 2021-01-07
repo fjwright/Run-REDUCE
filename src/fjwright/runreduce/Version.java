19cf9522a8e5fa94b420cd419d72b0b53275f092
Francis Wright <59773998+fjwright@users.noreply.github.com>
2021-01-07 17:22:15 +0000

    Add a main method to Version and use it to automate setting the app-version in the installer builders. This requires a new bash script for use with Cygwin.

--- src/fjwright/runreduce/Version.java
+++ src/fjwright/runreduce/Version.java
@@ -13,18 +13,25 @@ public class Version {
     private final static int YEAR = 2021;
 
     // Excess format arguments are ignored.
-    static final String VERSION = String.format(REVISION == 0 ? "%d.%d" : "%d.%d.%d",
+    public static final String VERSION = String.format(REVISION == 0 ? "%d.%d" : "%d.%d.%d",
             MAJOR, MINOR, REVISION);
     static final String DATE = String.format("%s %d", MONTH, YEAR);
     static final String COPYRIGHT = String.format("© 2020‒%d, Francis Wright", YEAR);
 
     static final String JAVA = String.format(
             "Compiled using: Java 14.0.2; JavaFX 15.0.1.\n" +
-            "Run using: Java %s; JavaFX %s.",
+                    "Run using: Java %s; JavaFX %s.",
             System.getProperty("java.version"),
             System.getProperty("javafx.version"));
 
 //    static {
 //        System.err.println(System.getProperties());
 //    }
+
+    /**
+     * This program is used to set the version in the installer builders.
+     */
+    public static void main(String[] args) {
+        System.out.println(Version.VERSION);
+    }
 }
