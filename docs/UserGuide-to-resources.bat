c868f943c9188485a4c169b2fd47bdfdede0ee82
Francis Wright <59773998+fjwright@users.noreply.github.com>
2023-10-29 18:43:45 +0000

    Automatically copy the User Guide from docs to resources only if it did not exist or was out of date in resources.

--- docs/UserGuide-to-resources.bat
+++ docs/UserGuide-to-resources.bat
@@ -1,1 +1,4 @@
-copy docs\UserGuide.html resources\
+@echo off
+rem Copy file only if source time newer than destination time.
+rem No prompting to overwrite an existing destination file.
+xcopy docs\UserGuide.html resources\ /D /Y
